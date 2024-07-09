b765edb8ba328d7c32ca36a388a08473ad7a5f5f
notchrulz1 <rulznotch@gmail.com>
2024-07-09 12:11:52 -0400

    Tank balancing 2.3

--- server/modules/definitions/gunvals.js
+++ server/modules/definitions/gunvals.js
@@ -1,6 +1,6 @@
 module.exports = {
 // Bases
-    basic: { reload: 10.5, recoil: 0.6, shudder: 0.1, damage: 0.45, speed: 5.5, spray: 15 },
+    basic: { reload: 10.5, recoil: 0.6, shudder: 0.1, damage: 0.55, speed: 5.5, spray: 15 },
     drone: { reload: 36, recoil: 0.25, shudder: 0.1, size: 0.6, speed: 2, spray: 0.1 },
     trap: { reload: 23, shudder: 0.05, size: 0.6, damage: 0.8, speed: 6.5, spray: 15, resist: 3 },
     swarm: { reload: 23.5, recoil: 0.35, shudder: 0.05, size: 0.4, damage: 2.4, speed: 4, spray: 5 },
@@ -28,7 +28,7 @@ module.exports = {
     hunterSecondary: { size: 0.9, health: 2, damage: 0.5, pen: 1.5, density: 1.2, resist: 1.1 },
     predator: { reload: 1.4, size: 0.8, health: 1.5, damage: 0.9, pen: 1.3, speed: 0.9, maxSpeed: 0.9 },
     dual: { reload: 2, shudder: 0.8, health: 1.5, speed: 1.3, maxSpeed: 1.1, resist: 1.25 },
-    rifle: { reload: 0.8, recoil: 0.8, shudder: 1.5, health: 0.8, damage: 0.8, pen: 0.9, spray: 2 },
+    rifle: { reload: 0.8, recoil: 0.8, shudder: 1.5, health: 0.8, damage: 1.15, pen: 0.9, spray: 2 },
     
 // Machine guns
     machineGun: { reload: 0.5, recoil: 0.8, shudder: 1.7, health: 0.7, damage: 1, maxSpeed: 0.8, spray: 2.5 },
@@ -37,7 +37,7 @@ module.exports = {
     nailgun: { reload: 1, recoil: 2.5, size: 0.8, damage: 0.9, density: 2, speed: 1.25, maxSpeed: 1.15 },
     pelleter: { reload: 1.25, recoil: 0.25, shudder: 1.5, size: 1.1, damage: 0.55, pen: 1.35, speed: 0.95, maxSpeed: 0.85, density: 1.5, spray: 1.5, resist: 1.2 },
     gunner: { recoil: 0.25, shudder: 1.5, size: 1.2, health: 1.35, damage: 0.25, pen: 1.25, speed: 0.8, maxSpeed: 0.65, density: 1.5, spray: 1.5, resist: 1.2 },
-    machineGunner: { reload: 0.66, recoil: 0.8, shudder: 0.2, damage: 1.3, speed: 1.2, maxSpeed: 0.8, spray: 0.2 },
+    machineGunner: { reload: 0.66, recoil: 0.8, shudder: 0.2, damage: 1.65, speed: 1.2, maxSpeed: 0.8, spray: 0.2 },
     blaster: { recoil: 1.2, shudder: 1.25, size: 1.1, health: 1.5, pen: 0.6, speed: 0.8, maxSpeed: 0.33, range: 0.6, density: 0.5, spray: 1.5, resist: 0.8 },
     focal: { reload: 1.25, recoil: 1.33, shudder: 0.8, health: 0.8, pen: 1.1, speed: 1.25, maxSpeed: 1.25, range: 1.1, density: 1.25, spray: 0.5, resist: 1.1 },
     atomizer: { reload: 0.3, recoil: 0.8, size: 0.5, damage: 0.75, speed: 1.2, maxSpeed: 0.8, spray: 2.25 },
@@ -88,8 +88,8 @@ module.exports = {
     rocketeerMissileTrail: { reload: 0.5, recoil: 1.4, shudder: 1.5, size: 0.8, health: 0.8, damage: 0.7, speed: 0.9, maxSpeed: 0.8, spray: 5 },
     
 // Traps and blocks
-    setTrap: { reload: 1.28, recoil: 1, shudder: 1, size: 1.5, health: 1.1, damage: 2, pen: 1.6, speed: 1.3, maxSpeed: 1.5, range: 1.25, resist: 1.25 },
-    construct: { reload: 1.3, size: 0.9, maxSpeed: 1.1 },
+    setTrap: { reload: 1.28, recoil: 1, shudder: 1, size: 1.5, health: 1.1, damage: 2, pen: 1.6, speed: 1.4, maxSpeed: 1.6, range: 1.25, resist: 1.25 },
+    construct: { reload: 1.3, size: 0.9, maxSpeed: 1.2 },
     boomerang: { reload: 0.8, health: 0.5, damage: 0.5, speed: 0.9, maxSpeed: 1.2, range: 1.333 },
     nestKeeper: { reload: 3, size: 0.75, health: 1.05, damage: 1.05, pen: 1.1, speed: 0.5, maxSpeed: 0.5, range: 0.5, density: 1.1 },
     hexaTrapper: { reload: 1.3, shudder: 1.25, speed: 0.8, range: 0.5 },
