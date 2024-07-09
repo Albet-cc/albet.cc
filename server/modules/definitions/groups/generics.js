d766808d90b74f8b0110f88eebf75f03093fda1e
notchrulz1 <rulznotch@gmail.com>
2024-07-08 22:48:13 -0400

    trap nerf

--- server/modules/definitions/groups/generics.js
+++ server/modules/definitions/groups/generics.js
@@ -280,7 +280,7 @@ Class.trap = {
     HITS_OWN_TYPE: "push",
     DIE_AT_RANGE: true,
     BODY: {
-        HEALTH: 0.5,
+        HEALTH: 0.2,
         PENETRATION: 0.4,
         DAMAGE: 3,
         RANGE: 450,
