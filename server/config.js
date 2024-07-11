e1d01011b1d23a38e3360da68760cef5e5aed90a
notchrulz1 <rulznotch@gmail.com>
2024-07-10 22:15:11 -0400

    size wall

--- server/config.js
+++ server/config.js
@@ -32,8 +32,8 @@ module.exports = {
     ROOM_SETUP: ['map_mazetdm_default'],
 
     // The dimensions of a single tile on the map.
-    TILE_WIDTH: 400,
-    TILE_HEIGHT: 400,
+    TILE_WIDTH: 800,
+    TILE_HEIGHT: 800,
 
 
 
