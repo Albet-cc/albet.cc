1e33e5dfbe6eb1986c9a58bd00e2e17b42e593a2
notchrulz1 <rulznotch@gmail.com>
2024-07-14 15:05:35 -0400

    forgot to save

--- server/modules/definitions/gunvals.js
+++ server/modules/definitions/gunvals.js
@@ -25,7 +25,7 @@ module.exports = {
     crossbow: { reload: 2.3, health: 0.6, damage: 1.5, pen: 0.9 },
     assassin: { reload: 1.65, shudder: 0.25, health: 0.9, damage: 1.35, pen: 0.6, speed: 1.18, maxSpeed: 1.18, density: 3, resist: 1.3 },
     hunter: { reload: 1.5, recoil: 0.7, size: 0.95, damage: 0.8, speed: 1.1, maxSpeed: 0.8, density: 1.2, resist: 1.15 },
-    hunterSecondary: { size: 0.9, health: 1.15, damage: 0.5, pen: 1, density: 1.2, resist: 1.1 },
+    hunterSecondary: { size: 0.9, health: 1.15, damage: 0.45, pen: 1, density: 1.2, resist: 1.1 },
     predator: { reload: 1.4, size: 0.8, health: 1.3, damage: 0.9, pen: 1.1, speed: 0.9, maxSpeed: 0.9 },
     dual: { reload: 2.8, shudder: 0.8, health: 1.5, speed: 1.3, damage: 1.3, maxSpeed: 1.1, resist: 1.25 },
     rifle: { reload: 0.9, recoil: 0.8, shudder: 1.5, health: 0.85, damage: 1.1, pen: 0.9, spray: 2 },
