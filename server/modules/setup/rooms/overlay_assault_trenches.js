5eac9e477a16751745f35bdf0d0f7017167d46c1
notchrulz1 <rulznotch@gmail.com>
2024-07-15 22:33:33 -0400

    i forgot

--- server/modules/setup/rooms/overlay_assault_trenches.js
+++ server/modules/setup/rooms/overlay_assault_trenches.js
@@ -1,4 +1,4 @@
-let { dominatorContested } = require('../tiles/dominators.js'),
+let { dominatorGreen, sanctuaryGreen } = require('../tiles/dominators.js'),
     room = Array(Config.roomHeight).fill(() => Array(Config.roomWidth).fill()).map(x => x());
 
 room[Math.floor(Config.roomHeight / 2)][Math.floor(Config.roomWidth / 4)] = 
