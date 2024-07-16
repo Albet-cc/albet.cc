604df8d7756d76d33dae7909856c7b14ad2f4311
notchrulz1 <rulznotch@gmail.com>
2024-07-16 10:23:27 -0400

    assault

--- server/modules/setup/rooms/overlay_assault_trenches.js
+++ server/modules/setup/rooms/overlay_assault_trenches.js
@@ -6,7 +6,7 @@ room[Math.floor(Config.roomHeight / 4)][Math.floor(Config.roomWidth / 2)] =
 room[Math.floor(3 * Config.roomHeight / 4)][Math.floor(Config.roomWidth / 2)] = 
 room[Math.floor(Config.roomHeight / 2)][Math.floor(3 * Config.roomWidth / 4)] =
 dominatorGreen; 
-room[Math.floor(Config.roomHeight / 1.12195121951)][Math.floor(Config.roomWidth / 1.09090909)] = sanctuaryGreen;
+room[Math.floor(Config.roomHeight / 1.15)][Math.floor(Config.roomWidth / 1.111111)] = sanctuaryGreen;
 
 
 module.exports = room;
