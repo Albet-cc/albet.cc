167bbbe24b4a65ef05b22b8d6c905f260f3da7f7
Taureon <45183108+Taureon@users.noreply.github.com>
2024-07-21 07:28:46 +0200

    Update webServer.js

--- server/modules/network/webServer.js
+++ server/modules/network/webServer.js
@@ -30,15 +30,16 @@ if (Config.host.match(/localhost:(\d)/) && Config.host !== 'localhost:' + Config
 server = require('http').createServer((req, res) => {
 
     //Enable CORS for *.dakarr.cc domains.
-    let origin = req.getHeader('origin');
-    console.log(req.hostname, req.host);
-    if (['us.dakarr.cc', 'eu.dakarr.cc', 'dakarr.cc'].includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-        if (req.method === 'OPTIONS') {
-            res.writeHead(200);
-            return res.end();
-        }
-    }
+    console.log(request.getHeaderNames().join(', '));
+    // let origin = req.getHeader('origin');
+    // console.log(origin);
+    // if (['us.dakarr.cc', 'eu.dakarr.cc', 'dakarr.cc'].includes(origin)) {
+    //     res.setHeader('Access-Control-Allow-Origin', origin);
+    //     if (req.method === 'OPTIONS') {
+    //         res.writeHead(200);
+    //         return res.end();
+    //     }
+    // }
 
     let resStr = "";
     if (req.url.startsWith('/shared/')) {
