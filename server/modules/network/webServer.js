013129b10a19e1c61244b87fa021e499d5f08760
Taureon <45183108+Taureon@users.noreply.github.com>
2024-07-21 07:26:54 +0200

    Update webServer.js

--- server/modules/network/webServer.js
+++ server/modules/network/webServer.js
@@ -30,7 +30,7 @@ if (Config.host.match(/localhost:(\d)/) && Config.host !== 'localhost:' + Config
 server = require('http').createServer((req, res) => {
 
     //Enable CORS for *.dakarr.cc domains.
-    let origin = req.get('origin');
+    let origin = req.getHeader('origin');
     console.log(req.hostname, req.host);
     if (['us.dakarr.cc', 'eu.dakarr.cc', 'dakarr.cc'].includes(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
