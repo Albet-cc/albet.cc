33481ac8fe66cbe4ee131cb3e5d7a903c2130e47
Taureon <45183108+Taureon@users.noreply.github.com>
2024-07-22 17:38:19 +0200

    Update tdm.js

--- server/modules/setup/gamemodeconfigs/tdm.js
+++ server/modules/setup/gamemodeconfigs/tdm.js
@@ -1,5 +1,9 @@
 module.exports = {
     MODE: "tdm",
     TEAMS: 4,
+    TILE_WIDTH: 800,
+    TILE_HEIGHT: 800,
+    FOOD_CAP_NEST: 3,
+    FOOD_SPAWN_COOLDOWN_NEST: 20,
     ROOM_SETUP: ['overlay_tdm']
 };
