f0ac858c8c8151099e1fc28fbad5bbd75997b499
Taureon <45183108+Taureon@users.noreply.github.com>
2024-07-25 19:53:23 +0200

    Update global.js

--- server/modules/global.js
+++ server/modules/global.js
@@ -173,7 +173,7 @@ global.foodSpawnProtection = entity => {
     entity.noclip = true;
     entity.layerID = 99;
     entity.skipLife = true;
-    if (limit-- && entity.skill.score - entity.skill.deduction >= entity.skill.levelScore) {
+    if (entity.skill.score - entity.skill.deduction >= entity.skill.levelScore) {
         entity.skill.deduction += entity.skill.levelScore;
         entity.skill.level += 1;
         entity.skill.points += entity.skill.levelPoints;
