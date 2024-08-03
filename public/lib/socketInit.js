05f2950c468a4509d701fbb3eb8bf0a9a0079c6d
Taureon <45183108+Taureon@users.noreply.github.com>
2024-08-03 17:30:45 +0200

    changed confusing default message

--- public/lib/socketInit.js
+++ public/lib/socketInit.js
@@ -799,7 +799,7 @@ const socketInit = port => {
     // This is for when the socket first opens
     socket.onopen = function socketOpen() {
         socket.open = true;
-        global.message = 'That token is invalid, expired, or already in use on this server. Please try another one!';
+        global.message = 'This server is restarting. Please try again in 5 seconds!';
         socket.talk('k', global.playerKey);
         console.log('Token submitted to the server for validation.');
         // define a pinging function
